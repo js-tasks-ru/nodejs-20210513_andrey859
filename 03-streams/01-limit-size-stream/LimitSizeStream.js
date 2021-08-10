d78cf602ef74dbb540a0912a03ff79ed4903f43f
An Ki <kiyko.andriy@gmail.com>
2021-08-10 22:48:23 +0200

    3rd lesson 2nd task LIneSplitStream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -11,6 +11,7 @@ class LimitSizeStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    console.log(chunk.toString())
     let error;
     try {
       if (chunk.length <= this.#limit) {
